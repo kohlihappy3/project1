2b9199383d42c2e82a34cb870036b2ba8475e329
HappyKohli <kohlihappy3@gmail.com>
2018-02-06 21:16:10 +0530

    Project1 completed

--- DirectoryListing_Software/src/Work.java
+++ DirectoryListing_Software/src/Work.java
@@ -1,17 +1,17 @@
 import java.io.*;
 import java.util.*;
 public class Work {
-	public static void list(String Dir){
-		
+	
+	
+	public static void list(String Dir, ArrayList<File> files){
 		File directory =new File(Dir);
 		File[] filelist=directory.listFiles();
 		for(File file : filelist){
 			if(file.isFile()){														//checks that the file is file or directory
 				System.out.println("File Name : "+file.getName()+"   Path : "+file.getPath());
-				
-				
+				files.add(file);
 				}else if(file.isDirectory()){										//if it is directory then recall function
-				list(file.getAbsolutePath());
+				list(file.getAbsolutePath(),files);
 			}
 		}
 	}
@@ -20,51 +20,48 @@ public class Work {
 	public static void main(String[] args) throws IOException{
 		ArrayList<File> files=new ArrayList<File>();
 		Scanner s = new Scanner(System.in);
-		System.out.println("Enter the path of file: ");						//taking the root file path from user
+		System.out.println("Enter the path of file: ");								//taking the root file path from user
 		String path=s.nextLine();
 		File file = new File(path);
 		FileReader f=null;
 		String path1="";
 		String path2="";
-		//	File res=new File(path2);
-			try{
+		try{
 			int c;
 			f= new FileReader(file);
 			int count=0;
 			while((c=f.read())!=10){
 				if((c)==10)
 				{
-					count++;								//C:\Users\Happy Kohli\Desktop
+					count++;								
 				}if(count==0){
-					path1=path1+(char)c;					//getting the first path of file
+					path1=path1+(char)c;											//getting the first path of file
 				}
 					}
    
-		   list(path1.substring(0,path1.length()-1));			//calling the function
+		   list(path1.substring(0,path1.length()-1),files);								//calling the function
 			
 		   while((c=f.read())!=-1){
 				path2=path2+(char)c;
 			}
 			
-		   File OFile=new File(path2);
-			FileWriter out=new FileWriter(OFile,true);
-			for(int i=0;i<files.size();i++){
-				out.write(files.get(i).getName());
-				out.write(files.get(i).getAbsolutePath());				//writing files in .csv files
-				out.write(10);
-			}
-			
-			out.flush();												//neutralizing resources
-			f.close();
-			s.close();
-						
-		}
+		    FileOutputStream  fileout=new FileOutputStream(path2,true);
+		    BufferedWriter b=new BufferedWriter(new OutputStreamWriter(fileout));
+		    Iterator<File> itr=files.iterator();
+		    	while(itr.hasNext()){
+		    	File temp=itr.next();
+				b.write("File Name :- " + temp.getName());
+				b.newLine();
+				b.write("The Path :- " + temp.getAbsolutePath());					//writing files in .csv files
+				b.newLine();
+				b.newLine();
+		    	}
+																
+			f.close();																//neutralizing resources		
+				}
+		
 			catch(Exception e){
 			System.out.println("Error Detected:- File Not Exist.");
-		}
-			
-			finally{
-			
 		}
 	}
 }
